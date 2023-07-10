e4f7067e114c3a982c10dcb524e2426625b29c08
Nermin Bakır <nrmn.bkr.07@gmail.com>
2023-07-10 12:55:28 +0300

    07/07/2023

---
+++ src/test/java/techproed/odev/Odev_ReadExcel02.java
@@ -0,0 +1,14 @@
+package techproed.odev;
+
+public class Odev_ReadExcel02 {
+
+    //    Yeni bir test method olusturalim writeExcelTest()
+    //    Adimlari takip ederek 1.satira kadar gidelim
+    //    5.hucreye yeni bir cell olusturalim
+    //    Olusturdugumuz hucreye “Nufus” yazdiralim
+    //    2.satir nufus kolonuna 1500000 yazdiralim
+    //    10.satir nufus kolonuna 250000 yazdiralim
+    //    15.satir nufus kolonuna 54000 yazdiralim
+    //    Dosyayi kaydedelim
+    //    Dosyayi kapatalim
+}
